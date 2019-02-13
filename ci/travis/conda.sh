661fda24288102e655c367b1b080e9d53953a227
Allan Leal <allanmulin@gmail.com>
2019-02-13 09:55:05 +0100

    Update conda install script for travis

--- ci/travis/conda.sh
+++ ci/travis/conda.sh
@@ -1,17 +1,10 @@
-# Download Linux miniconda installer
-if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
-  wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
-else
-  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
-fi
-
-# Install miniconda in silent mode
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then OS=Linux-x86_64; else OS=MacOSX-x86_64; fi
+wget -O miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-$OS.sh
 bash miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
-hash -r
+bash $HOME/miniconda/etc/profile.d/conda.sh
+export PATH=$HOME/miniconda/bin/:$PATH
 conda config --set always_yes yes --set changeps1 no
-conda update --quiet conda
+conda config --add channels conda-forge
+conda install conda-devenv
+conda update -q conda
 conda info -a
-
-# Install some conda development tools
-conda install conda-forge::conda-devenv
