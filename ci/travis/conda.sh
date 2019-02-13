ddf44aae0ad96a4ec5333d7e558752084ddbc154
Allan Leal <allanmulin@gmail.com>
2019-02-13 06:55:06 +0100

    Add travis support

---
+++ ci/travis/conda.sh
@@ -0,0 +1,12 @@
+if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
+  wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
+else
+  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+fi
+
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda update --quiet conda
+conda info -a
