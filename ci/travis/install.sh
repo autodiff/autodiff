1efd09f6dc008c7dfde2495ef59989cf37ab5fa0
Allan Leal <allanmulin@gmail.com>
2019-02-13 10:49:13 +0100

    Update install.sh

--- ci/travis/install.sh
+++ ci/travis/install.sh
@@ -1,11 +1,11 @@
-if [ ! $HOME/miniconda/bin/conda ]; then
+if [ ! -f $HOME/miniconda/bin/conda ]; then
     echo "Downloading and installing miniconda"
     if [ $TRAVIS_OS_NAME = "linux" ]; then OS=Linux-x86_64; else OS=MacOSX-x86_64; fi
     wget -O miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-$OS.sh
     rm -rf $HOME/miniconda
     bash miniconda.sh -b -p $HOME/miniconda
 fi
-if [ ! -f $HOME/miniconda/bin/conda ]
+if [ ! -f $HOME/miniconda/bin/conda ]; then
     echo ERROR: conda was not installed.
     exit 1
 fi
