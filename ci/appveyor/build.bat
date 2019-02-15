b802e0739cd3b9db4a070808af1a55871635b8fe
Allan Leal <allanmulin@gmail.com>
2019-02-15 13:21:03 +0100

    Fix init of Visual Studion vcvars

--- ci/appveyor/build.bat
+++ ci/appveyor/build.bat
@@ -1,6 +1,14 @@
+if "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2017" (
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+)
+if "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2015" (
+    call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
+    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
+)
+
 mkdir build
 cd build
 echo "Configuring..."
-cmake .. -GNinja
+cmake ..
 echo "Building..."
-ninja
+cmake --build build --target install
