d8781970a47f3fb6a354161e1b29ff193bba2a77
Allan Leal <allanmulin@gmail.com>
2019-06-18 10:21:21 +0200

    Fix cmake example

--- examples/cmake-project/main.cpp
+++ examples/cmake-project/main.cpp
@@ -13,7 +13,7 @@ int main()
 {
     dual x = 1.0;
     dual u = f(x);
-    double dudx = derivative(f, wrt(x), x);
+    double dudx = derivative(f, wrt(x), at(x));
 
     cout << "u = " << u << endl;
     cout << "du/dx = " << dudx << endl;
