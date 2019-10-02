6474dc75537ec5cfd0f3de06470558072af6aee3
supersega <49091440+supersega@users.noreply.github.com>
2019-10-02 16:41:47 +0300

    [autodiff/forward] Simplify higher order derivatives (#64)
    * [autodiff/forward] add helper function to repeat argument to tuple
    
    * [autodiff/forward/examples] add overloading of wrt function which will simplify heigher order derivatives
    
    * [autodiff/tests] use new wrt overloading in tests

--- examples/forward/example-forward-higher-order-derivatives.cpp
+++ examples/forward/example-forward-higher-order-derivatives.cpp
@@ -24,10 +24,10 @@ int main()
     dual ux = derivative(f, wrt(x), at(x, y));  // evaluate the derivative du/dx
     dual uy = derivative(f, wrt(y), at(x, y));  // evaluate the derivative du/dy
 
-    double uxx = derivative(f, wrt(x, x), at(x, y));  // evaluate the derivative d²u/dxdx
+    double uxx = derivative(f, wrt<2>(x), at(x, y));  // evaluate the derivative d²u/dxdx
     double uxy = derivative(f, wrt(x, y), at(x, y));  // evaluate the derivative d²u/dxdy
     double uyx = derivative(f, wrt(y, x), at(x, y));  // evaluate the derivative d²u/dydx
-    double uyy = derivative(f, wrt(y, y), at(x, y));  // evaluate the derivative d²u/dydy
+    double uyy = derivative(f, wrt<2>(y), at(x, y));  // evaluate the derivative d²u/dydy
 
     cout << "u   = " << u << endl;    // print the evaluated output u
     cout << "ux  = " << ux << endl;   // print the evaluated derivative du/dx
