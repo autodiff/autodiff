ebbb8ae5566766fc41f219da8e7b47d298dec0d0
Allan Leal <allan.leal@erdw.ethz.ch>
2019-11-01 14:36:05 +0100

    Clean taylorseries file

--- autodiff/forward/common/taylorseries.hpp
+++ autodiff/forward/common/taylorseries.hpp
@@ -33,26 +33,12 @@
 #include <array>
 
 // autodiff includes
+#include <autodiff/forward/common/derivative.hpp>
+#include <autodiff/utils/aliases.hpp>
 #include <autodiff/utils/meta.hpp>
 
 namespace autodiff {
 namespace detail {
-
-template<typename Item>
-auto _wrt_item_length(const Item& item) -> size_t
-{
-    if constexpr (hasSize<Item>)
-        return item.size();
-    else return 1;
-}
-
-template<typename... Items>
-auto _wrt_total_length(const std::tuple<Items...>& items)
-{
-    return Reduce(items, [&](auto&& item) constexpr {
-        return _wrt_item_length(item);
-    });
-}
 
 template<size_t N, typename U>
 class TaylorSeries
@@ -86,44 +72,9 @@ private:
     std::array<N + 1, U> _data;
 };
 
-template<typename At, typename Along>
-auto seed(const At& at, const Along& along)
-{
-    static_assert(At::numArgs == Along::numArgs);
-    constexpr auto M = At::numArgs;
-
-    For<M>([&](auto&& i) constexpr {
-        std::get<i.index>(at.args)
-    });
-
-    ForEach(wrt.args, [&](auto& item) constexpr
-    {
-        if constexpr (hasSize<decltype(item)>)
-        {
-            const size_t len = item.size();
-            for(size_t j = 0; j < len; ++j)
-            {
-                seed(item[j]);
-                u = std::apply(f, at.args);
-                unseed(item[j]);
-                g[offset + j] = derivative<1>(u);
-            }
-            offset += len;
-        }
-        else
-        {
-            seed(item);
-            u = std::apply(f, at.args);
-            unseed(item);
-            g[offset] = derivative<1>(u);
-            ++offset;
-        }
-    });
-}
-
 /// Return the gradient of scalar function *f* with respect to some or all variables *x*.
-template<typename Function, typename Along, typename At>
-auto taylorseries(const Function& f, const Along& along, const At& at)
+template<typename Fun, typename...Vecs, typename... Args>
+auto taylorseries(const Fun& f, const Along<Vecs...>& along, const At<Args&...>& at)
 {
     static_assert(Wrt::numArgs);
     static_assert(At::numArgs);
