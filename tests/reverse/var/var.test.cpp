e589ae5c69795a2c5eb7020cafb834a23487a50a
Allan Leal <allanmulin@gmail.com>
2019-11-05 19:59:09 +0100

    Update year

--- tests/reverse/var/var.test.cpp
+++ tests/reverse/var/var.test.cpp
@@ -7,7 +7,7 @@
 //
 // Licensed under the MIT License <http://opensource.org/licenses/MIT>.
 //
-// Copyright (c) 2018-2019 Allan Leal
+// Copyright (c) 2018-2020 Allan Leal
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
