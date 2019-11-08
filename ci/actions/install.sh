99e14b31fe121a9a0ac71385c14fb16808d9a7b2
Diego Volpatto <diego@esss.co>
2019-11-08 10:10:40 -0300

    Create install.sh

---
+++ ci/actions/install.sh
@@ -0,0 +1,14 @@
+
+bash $HOME/miniconda/etc/profile.d/conda.sh
+export PATH=$HOME/miniconda/bin/:$PATH
+conda config --set always_yes yes --set changeps1 no
+conda config --add channels conda-forge
+conda install conda-devenv
+conda update -q conda
+conda info -a
+conda devenv
+source activate autodiff
+mkdir build
+cd build
+cmake .. -GNinja
+ninja
