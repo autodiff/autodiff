ecafcffc50709a9fedd9692df9241b227ced601e
Allan Leal <allan.leal@erdw.ethz.ch>
2019-11-08 11:57:19 +0100

    Returning to previous appveyor config

--- ci/appveyor/install.bat
+++ ci/appveyor/install.bat
@@ -7,6 +7,3 @@ conda devenv
 echo "Activating the conda environment..."
 call activate autodiff
 echo "Activating the conda environment...finished!"
-
-REM Call this after activating the conda env to ensure MSVC 2017 is selected.
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
