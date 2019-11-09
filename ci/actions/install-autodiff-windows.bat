d3891f0e194130017010dbfd289c6fd35742cf80
Allan Leal <allanmulin@gmail.com>
2019-11-09 09:35:32 +0100

    Using ninja in actions for windows (as in the feedstock conda recipe, which is working).

--- ci/actions/install-autodiff-windows.bat
+++ ci/actions/install-autodiff-windows.bat
@@ -2,11 +2,11 @@ set PATH=%CONDA%;%CONDA%\Scripts;%CONDA%\Library\bin;%PATH% || goto :error
 call activate autodiff || goto :error
 
 echo === Configuring autodiff...
-cmake -S . -B build || goto :error
+cmake -S . -B build -GNinja || goto :error
 echo === Configuring autodiff...finished!
 
 echo === Building and installing autodiff...
-cmake --build build --config %CONFIGURATION% --target install || goto :error
+ninja install || goto :error
 echo === Building and installing autodiff...finished!
 
 exit /B 0
