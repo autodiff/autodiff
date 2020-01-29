b54fb0d564bb04f89127957de9a5d1eb087ed80d
volpatto <volpatto@lncc.br>
2020-01-29 17:19:59 -0300

    Remove manual conda path setting

--- ci/actions/install.sh
+++ ci/actions/install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-bash "$HOME"/miniconda/etc/profile.d/conda.sh
-export PATH=$HOME/miniconda/bin/:$PATH
 conda config --set always_yes yes --set changeps1 no
 conda config --add channels conda-forge
 conda install conda-devenv
