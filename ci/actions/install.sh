73cf7fec9e829380e12fa6800e82e53eab749c1b
Diego Volpatto <diego@esss.co>
2020-01-30 04:15:05 -0300

    Use built-in miniconda from GH action (#98)
    * Fix by pre-commit and shebang in shell scripts for portability
    
    * Remove manual conda path setting
    
    * Update linux.yml with conda action
    
    * Change "source" by "conda" in install.sh
    
    * Try to set bash through action steps
    
    * Try to restore source instead conda
    
    * Set macOS build with conda from actions
    
    * Add conda from actions in win build
    
    * Update windows.yml
    
    * Update windows.yml
    
    * Try to change powershell to cmd
    
    * Restore pipe in the cmd execution
    
    * Display envs before build
    
    * Add conda devenv install step
    
    * Add conda-forge channel
    
    * Update windows.yml
    
    * Display envs in order to check if autodiff is there
    
    * Update environment.devenv.yml with conda-forge
    
    * Update windows.yml
    
    * Try powershell instead cmd
    
    * Update windows.yml
    
    * Update windows.yml
    
    * Update windows.yml
    
    * Try to use devenv file
    
    * Update windows.yml
    
    * Update windows.yml
    
    * Update windows.yml
    
    * Fix powershell tests run
    
    * Update windows.yml
    
    * A simple test to check Action
    
    * More Action test
    
    * Update windows.yml
    
    * Fix windows build

--- ci/actions/install.sh
+++ ci/actions/install.sh
@@ -1,6 +1,5 @@
+#!/usr/bin/env bash
 
-bash $HOME/miniconda/etc/profile.d/conda.sh
-export PATH=$HOME/miniconda/bin/:$PATH
 conda config --set always_yes yes --set changeps1 no
 conda config --add channels conda-forge
 conda install conda-devenv
@@ -9,6 +8,6 @@ conda info -a
 conda devenv
 source activate autodiff
 mkdir build
-cd build
+cd build || exit
 cmake .. -GNinja
 ninja
--- ci/actions/test.sh
+++ ci/actions/test.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env bash
+
 ./build/test/tests
