d3619aeee830f33e7d3a6aa309f6c688b5ea0403
Allan Leal <allan.leal@erdw.ethz.ch>
2020-09-25 11:51:57 +0200

    Update CondaAware.cmake

--- cmake/CondaAware.cmake
+++ cmake/CondaAware.cmake
@@ -19,13 +19,25 @@ if(DEFINED ENV{CONDA_PREFIX})
     message(STATUS "CondaAware: conda environment recognized!")
     message(STATUS "CondaAware: CONDA_PREFIX=$ENV{CONDA_PREFIX}")
 
+    # Set python executable to that in the conda environment (unix)
+    if(UNIX AND DEFINED ENV{CONDA_PYTHON_EXE})
+        set(PYTHON_EXECUTABLE "$ENV{CONDA_PREFIX}/bin/python")
+        message(STATUS "CondaAware: PYTHON_EXECUTABLE=CONDA_PREFIX/bin/python (${PYTHON_EXECUTABLE})")
+    endif()
+
+    # Set python executable to that in the conda environment (unix)
+    if(WIN32 AND DEFINED ENV{CONDA_PYTHON_EXE})
+        set(PYTHON_EXECUTABLE "$ENV{CONDA_PREFIX}\\python.exe")
+        message(STATUS "CondaAware: PYTHON_EXECUTABLE=CONDA_PREFIX\\python.exe (${PYTHON_EXECUTABLE})")
+    endif()
+
     # Check if in Unix and not in a conda build task
     if(UNIX AND NOT DEFINED ENV{CONDA_BUILD})
         set(CONDA_AWARE_PREFIX "$ENV{CONDA_PREFIX}")
         message(STATUS "CondaAware: CONDA_AWARE_PREFIX=CONDA_PREFIX (${CONDA_AWARE_PREFIX})")
     endif()
 
-    # Check if in Unix and not in a conda build task
+    # Check if in Windows and not in a conda build task
     if(WIN32 AND NOT DEFINED ENV{CONDA_BUILD})
         set(CONDA_AWARE_PREFIX "$ENV{CONDA_PREFIX}\\Library")
         message(STATUS "CondaAware: CONDA_AWARE_PREFIX=CONDA_PREFIX\\Library (${CONDA_AWARE_PREFIX})")
@@ -52,19 +64,19 @@ if(DEFINED ENV{CONDA_PREFIX})
 
     # Set CMAKE_INSTALL_PREFIX to CONDA_AWARE_PREFIX if not specified by the user
     if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
-        message(STATUS "CondaAware: CMAKE_INSTALL_PREFIX set to CONDA_AWARE_PREFIX")
+        message(STATUS "CondaAware: CMAKE_INSTALL_PREFIX set to CONDA_AWARE_PREFIX (${CONDA_AWARE_PREFIX})")
         set(CMAKE_INSTALL_PREFIX ${CONDA_AWARE_PREFIX})
     endif()
 
     # Ensure dependencies from the conda environment are used instead of those from the system.
-    message(STATUS "CondaAware: appended CONDA_AWARE_PREFIX to CMAKE_PREFIX_PATH")
+    message(STATUS "CondaAware: appended ${CONDA_AWARE_PREFIX} to CMAKE_PREFIX_PATH")
     list(APPEND CMAKE_PREFIX_PATH ${CONDA_AWARE_PREFIX})
 
     # Ensure include directory in conda environment is known to the project
-    message(STATUS "CondaAware: appended CONDA_AWARE_PREFIX/include to include directories")
+    message(STATUS "CondaAware: appended ${CONDA_AWARE_PREFIX}/include to include directories")
     include_directories(${CONDA_AWARE_PREFIX}/include)
 
     # Ensure library directory in conda environment is known to the project
-    message(STATUS "CondaAware: appended CONDA_AWARE_PREFIX/lib to link directories")
+    message(STATUS "CondaAware: appended ${CONDA_AWARE_PREFIX}/lib to link directories")
     link_directories(${CONDA_AWARE_PREFIX}/lib)
 endif()
