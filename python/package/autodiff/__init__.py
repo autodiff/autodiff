58e5032221ee7abf79a24f92fe160d74c8d2e6f3
Allan Leal <allanmulin@gmail.com>
2023-03-15 16:10:27 +0100

    Implemented  __format__ method for exported Python bindings for real types

--- python/package/autodiff/__init__.py
+++ python/package/autodiff/__init__.py
@@ -1,2 +1,12 @@
+from autodiff import autodiff4py
+
+def autodiff_number_format(self, spec):
+    return format(self.val(), spec)
+
+autodiff4py.real1st.__format__ = autodiff_number_format
+autodiff4py.real2nd.__format__ = autodiff_number_format
+autodiff4py.real3rd.__format__ = autodiff_number_format
+autodiff4py.real4th.__format__ = autodiff_number_format
+
 from autodiff.autodiff4py import *
 # from autodiff._extensions.some_module import *
