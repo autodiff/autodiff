5c995b1346dd9744a34506f6a439f4a0b82e93d9
Allan Leal <allanmulin@gmail.com>
2023-03-16 08:58:02 +0100

    Implemented __format__ method in the Python API of real types to permit formatted strings with real.

--- python/package/autodiff/__init__.py
+++ python/package/autodiff/__init__.py
@@ -1,12 +1,24 @@
 from autodiff import autodiff4py
 
-def autodiff_number_format(self, spec):
+# List of all autodiff C++ number types exported to Python. Extend this as other
+# number types are exported!
+_autodiff_number_types = [
+    autodiff4py.real1st,
+    autodiff4py.real2nd,
+    autodiff4py.real3rd,
+    autodiff4py.real4th,
+]
+
+# -------------------------------------------------------------------------------------
+# Define the __format__ methods for all autodiff number types. This is needed so
+# that we can write formatted strings such as f"{x:.3f}" and avoid a Python
+# runtime error.
+def _autodiff_number_format(self, spec):
     return format(self.val(), spec)
 
-autodiff4py.real1st.__format__ = autodiff_number_format
-autodiff4py.real2nd.__format__ = autodiff_number_format
-autodiff4py.real3rd.__format__ = autodiff_number_format
-autodiff4py.real4th.__format__ = autodiff_number_format
+for numbertype in _autodiff_number_types:
+    numbertype.__format__ = _autodiff_number_format
+
 
 from autodiff.autodiff4py import *
 # from autodiff._extensions.some_module import *
